c5bd6bc2a8e6dffad4ad044a8a74f28dde1afe3e
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-02-28 00:22:09 +0400

    third commit

---
+++ tasks.dart
@@ -0,0 +1,5 @@
+// methodlarla parameter gondererek calculator yazin  
+// cut ve tek ededleri teyin edecek proqram yazin (return type metod ile) -- istifadeci ededi daxil etsin
+// get type method yazin hansi ki , bu method vasitesile biz ededin faktorialini tapa bilek (faktorial - daxil edilen ededlerin bir birine hasili)
+// Verilmiş iki tam ədədin birinin müsbət, digərinin isə mənfi olub olmadığını yoxlamaq üçün dart proqramı yazın.
+// toplama , cıxma , bölmə , vurma kimi opreasiyalari apara bilecek metod yaradin. (null safety , required data  , appointed - bu tiplerde olsun)
