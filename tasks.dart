3f53301f632f254c41bb0622cd59e449352befb4
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-02-28 00:43:50 +0400

    third commit

--- tasks.dart
+++ tasks.dart
@@ -3,3 +3,12 @@
 // get type method yazin hansi ki , bu method vasitesile biz ededin faktorialini tapa bilek (faktorial - daxil edilen ededlerin bir birine hasili)
 // Verilmiş iki tam ədədin birinin müsbət, digərinin isə mənfi olub olmadığını yoxlamaq üçün dart proqramı yazın.
 // toplama , cıxma , bölmə , vurma kimi opreasiyalari apara bilecek metod yaradin. (null safety , required data  , appointed - bu tiplerde olsun)
+/*
+Tələbənin daxil etdiyi bala görə ABCDEF
+qiymətlərindən hansını aldığını
+müəyyənləşdirən proqram yazın. 91-100 
+üçün "A", 81-89 üçün "B", 71-79 üçün "C", 61-
+69 üçün "D", 51-59 üçün "E"  daha așağı qiymətlər üçün
+"F". Daxil edilən rəqəm müsbət, sifırdan böyük
+və 100-dən kiçik olmalıdır. method istifade ederek hell edin
+ */
